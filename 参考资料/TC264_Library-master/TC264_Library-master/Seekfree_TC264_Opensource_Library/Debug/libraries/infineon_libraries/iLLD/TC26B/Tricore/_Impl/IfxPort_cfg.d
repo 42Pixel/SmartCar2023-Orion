70da09420efbf930696939a02db48461573cfd19
Cole-20 <JayLee_42@outlook.com>
2023-04-08 21:48:21 +0800

    commit

---
+++ 参考资料/TC264_Library-master/TC264_Library-master/Seekfree_TC264_Opensource_Library/Debug/libraries/infineon_libraries/iLLD/TC26B/Tricore/_Impl/IfxPort_cfg.d
@@ -0,0 +1,32 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+libraries/infineon_libraries/iLLD/TC26B/Tricore/_Impl/IfxPort_cfg.o :	../libraries/infineon_libraries/iLLD/TC26B/Tricore/_Impl/IfxPort_cfg.c
+../libraries/infineon_libraries/iLLD/TC26B/Tricore/_Impl/IfxPort_cfg.c :
+libraries/infineon_libraries/iLLD/TC26B/Tricore/_Impl/IfxPort_cfg.o :	../libraries/infineon_libraries/iLLD/TC26B/Tricore/_Impl/IfxPort_cfg.h
+../libraries/infineon_libraries/iLLD/TC26B/Tricore/_Impl/IfxPort_cfg.h :
+libraries/infineon_libraries/iLLD/TC26B/Tricore/_Impl/IfxPort_cfg.o :	e:/Git-Files/Fastest-Car/参考资料/TC264_Library-master/TC264_Library-master/Seekfree_TC264_Opensource_Library/libraries/infineon_libraries/iLLD/TC26B/Tricore/Cpu/Std/IfxCpu_Intrinsics.h
+e:/Git-Files/Fastest-Car/参考资料/TC264_Library-master/TC264_Library-master/Seekfree_TC264_Opensource_Library/libraries/infineon_libraries/iLLD/TC26B/Tricore/Cpu/Std/IfxCpu_Intrinsics.h :
+libraries/infineon_libraries/iLLD/TC26B/Tricore/_Impl/IfxPort_cfg.o :	e:/Git-Files/Fastest-Car/参考资料/TC264_Library-master/TC264_Library-master/Seekfree_TC264_Opensource_Library/libraries/infineon_libraries/iLLD/TC26B/Tricore/Cpu/Std/Ifx_Types.h
+e:/Git-Files/Fastest-Car/参考资料/TC264_Library-master/TC264_Library-master/Seekfree_TC264_Opensource_Library/libraries/infineon_libraries/iLLD/TC26B/Tricore/Cpu/Std/Ifx_Types.h :
+libraries/infineon_libraries/iLLD/TC26B/Tricore/_Impl/IfxPort_cfg.o :	e:/Git-Files/Fastest-Car/参考资料/TC264_Library-master/TC264_Library-master/Seekfree_TC264_Opensource_Library/libraries/infineon_libraries/Infra/Platform/Tricore/Compilers/Compilers.h
+e:/Git-Files/Fastest-Car/参考资料/TC264_Library-master/TC264_Library-master/Seekfree_TC264_Opensource_Library/libraries/infineon_libraries/Infra/Platform/Tricore/Compilers/Compilers.h :
+libraries/infineon_libraries/iLLD/TC26B/Tricore/_Impl/IfxPort_cfg.o :	e:/Git-Files/Fastest-Car/参考资料/TC264_Library-master/TC264_Library-master/Seekfree_TC264_Opensource_Library/libraries/infineon_libraries/Configurations/Ifx_Cfg.h
+e:/Git-Files/Fastest-Car/参考资料/TC264_Library-master/TC264_Library-master/Seekfree_TC264_Opensource_Library/libraries/infineon_libraries/Configurations/Ifx_Cfg.h :
+libraries/infineon_libraries/iLLD/TC26B/Tricore/_Impl/IfxPort_cfg.o :	e:/Git-Files/Fastest-Car/参考资料/TC264_Library-master/TC264_Library-master/Seekfree_TC264_Opensource_Library/libraries/infineon_libraries/Infra/Platform/Tricore/Compilers/CompilerTasking.h
+e:/Git-Files/Fastest-Car/参考资料/TC264_Library-master/TC264_Library-master/Seekfree_TC264_Opensource_Library/libraries/infineon_libraries/Infra/Platform/Tricore/Compilers/CompilerTasking.h :
+libraries/infineon_libraries/iLLD/TC26B/Tricore/_Impl/IfxPort_cfg.o :	d:/ADS/plugins/com.infineon.aurix.tools_1.8.0/build_system/tools/Compilers/Tasking_1.1r8/ctc/include/stddef.h
+d:/ADS/plugins/com.infineon.aurix.tools_1.8.0/build_system/tools/Compilers/Tasking_1.1r8/ctc/include/stddef.h :
+libraries/infineon_libraries/iLLD/TC26B/Tricore/_Impl/IfxPort_cfg.o :	e:/Git-Files/Fastest-Car/参考资料/TC264_Library-master/TC264_Library-master/Seekfree_TC264_Opensource_Library/libraries/infineon_libraries/iLLD/TC26B/Tricore/Cpu/Std/Platform_Types.h
+e:/Git-Files/Fastest-Car/参考资料/TC264_Library-master/TC264_Library-master/Seekfree_TC264_Opensource_Library/libraries/infineon_libraries/iLLD/TC26B/Tricore/Cpu/Std/Platform_Types.h :
+libraries/infineon_libraries/iLLD/TC26B/Tricore/_Impl/IfxPort_cfg.o :	e:/Git-Files/Fastest-Car/参考资料/TC264_Library-master/TC264_Library-master/Seekfree_TC264_Opensource_Library/libraries/infineon_libraries/iLLD/TC26B/Tricore/Cpu/Std/Ifx_TypesTasking.h
+e:/Git-Files/Fastest-Car/参考资料/TC264_Library-master/TC264_Library-master/Seekfree_TC264_Opensource_Library/libraries/infineon_libraries/iLLD/TC26B/Tricore/Cpu/Std/Ifx_TypesTasking.h :
+libraries/infineon_libraries/iLLD/TC26B/Tricore/_Impl/IfxPort_cfg.o :	e:/Git-Files/Fastest-Car/参考资料/TC264_Library-master/TC264_Library-master/Seekfree_TC264_Opensource_Library/libraries/infineon_libraries/iLLD/TC26B/Tricore/Cpu/Std/IfxCpu_IntrinsicsTasking.h
+e:/Git-Files/Fastest-Car/参考资料/TC264_Library-master/TC264_Library-master/Seekfree_TC264_Opensource_Library/libraries/infineon_libraries/iLLD/TC26B/Tricore/Cpu/Std/IfxCpu_IntrinsicsTasking.h :
+libraries/infineon_libraries/iLLD/TC26B/Tricore/_Impl/IfxPort_cfg.o :	e:/Git-Files/Fastest-Car/参考资料/TC264_Library-master/TC264_Library-master/Seekfree_TC264_Opensource_Library/libraries/infineon_libraries/Infra/Sfr/TC26B/_Reg/IfxPort_reg.h
+e:/Git-Files/Fastest-Car/参考资料/TC264_Library-master/TC264_Library-master/Seekfree_TC264_Opensource_Library/libraries/infineon_libraries/Infra/Sfr/TC26B/_Reg/IfxPort_reg.h :
+libraries/infineon_libraries/iLLD/TC26B/Tricore/_Impl/IfxPort_cfg.o :	e:/Git-Files/Fastest-Car/参考资料/TC264_Library-master/TC264_Library-master/Seekfree_TC264_Opensource_Library/libraries/infineon_libraries/Infra/Sfr/TC26B/_Reg/IfxPort_regdef.h
+e:/Git-Files/Fastest-Car/参考资料/TC264_Library-master/TC264_Library-master/Seekfree_TC264_Opensource_Library/libraries/infineon_libraries/Infra/Sfr/TC26B/_Reg/IfxPort_regdef.h :
+libraries/infineon_libraries/iLLD/TC26B/Tricore/_Impl/IfxPort_cfg.o :	e:/Git-Files/Fastest-Car/参考资料/TC264_Library-master/TC264_Library-master/Seekfree_TC264_Opensource_Library/libraries/infineon_libraries/Infra/Sfr/TC26B/_Reg/Ifx_TypesReg.h
+e:/Git-Files/Fastest-Car/参考资料/TC264_Library-master/TC264_Library-master/Seekfree_TC264_Opensource_Library/libraries/infineon_libraries/Infra/Sfr/TC26B/_Reg/Ifx_TypesReg.h :
